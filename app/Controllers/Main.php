9c3ea04f8eeaaf35e2862859f1203be90c77a304
JOAO LUCAS TEIXEIRA MAGALHAES <joao.ltmagalhaes@senacsp.edu.br>
2025-01-23 17:30:07 -0300

    stuff

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -4,14 +4,16 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ProdutoModel;
 
 class Main extends BaseController
 {
     public function index()
     {
+        $produtos = new ProdutoModel();
         return view('templates/header') .
-               view('templates/navbar') .
-               view('index') .
-               view('templates/footer');
+        view('templates/navbar') .
+        view('index', ['produtos' => $produtos->findAll()]) .
+        view('templates/footer');
     }
 }
--- produtos.sql
+++ produtos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23/01/2025 às 20:51
+-- Tempo de geração: 23/01/2025 às 21:29
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -40,10 +40,10 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `produto`, `descricao`, `preco`, `imagem`) VALUES
-(1, 'Notebook', 'É um Notebook Bom', 3000.00, '1737654897_a1b8798e8f7d46c3b08e.jpg'),
-(2, 'Xbox Series X', 'É um Videogame Bom', 4000.00, '1737657025_0ed14d0f03416bf0a13c.png'),
-(3, 'PS5', 'É um Videogame Bom', 4000.00, '1737657450_e118cb11d48e06256bf6.jpg'),
-(4, 'Nintendo Switch', 'É um Videogame Bom', 2000.00, '1737657759_0d0d13b9033073ca896e.jpg');
+(1, 'Notebook IdeaPad Gaming', 'O Notebook IdeaPad Gaming oferece desempenho potente, gráficos dedicados e design moderno, ideal para jogos e multitarefas com alta performance.', 3000.00, '1737654897_a1b8798e8f7d46c3b08e.jpg'),
+(2, 'Xbox Series X', 'O Xbox Series X é um console potente com gráficos 4K, carregamentos rápidos e acesso a uma vasta biblioteca de jogos pelo Game Pass.', 4000.00, '1737657025_0ed14d0f03416bf0a13c.png'),
+(3, 'PS5', 'O PS5 é um console de última geração com gráficos em 4K, carregamentos ultrarrápidos e uma vasta biblioteca de jogos exclusivos.', 4000.00, '1737657450_e118cb11d48e06256bf6.jpg'),
+(4, 'Nintendo Switch', 'O Switch é um console híbrido que oferece jogos portáteis e de mesa, com uma vasta biblioteca de títulos divertidos.', 2000.00, '1737657759_0d0d13b9033073ca896e.jpg');
 
 --
 -- Índices para tabelas despejadas
